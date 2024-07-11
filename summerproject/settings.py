28acd4b803fa20d453074036c4e829ed227674fa
Liam Chen“ <shuyic9@uci.edu>
2024-07-10 22:23:30 -0700

    update splash settings

--- summerproject/settings.py
+++ summerproject/settings.py
@@ -19,6 +19,28 @@ NEWSPIDER_MODULE = "summerproject.spiders"
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+# settings.py
+
+# Splash Server Endpoint
+SPLASH_URL = 'http://localhost:8050'
+
+
+# Enable Splash downloader middleware and change HttpCompressionMiddleware priority
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+# Enable Splash Deduplicate Args Filter
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+# Define the Splash DupeFilter
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
