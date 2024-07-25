95edea57342dca19651250f9d723070416859c9b
eriedaberrie <eriedaberrie@gmail.com>
2024-07-25 11:58:02 -0700

    Prevent CNN crawler from timing out

--- summerproject/spiders/cnn.py
+++ summerproject/spiders/cnn.py
@@ -1,20 +1,21 @@
 import scrapy
 import logging
-from scrapy_playwright.page import PageMethod
+import re
+import playwright
 
 
 class CnnSpider(scrapy.Spider):
     name = "cnn"
     start_urls = [
-        "https://www.cnn.com/search?q=palestine+israel+gaza&types=article",
+        "https://www.cnn.com/search?q=palestine+hamas+gaza&types=article",
     ]
     custom_settings = {
-        # "PLAYWRIGHT_LAUNCH_OPTIONS": {
-        #     "headless": False,
-        # },
-        "PLAYWRIGHT_DEFAULT_NAVIGATION_TIMEOUT": 60 * 1000,
+        "PLAYWRIGHT_LAUNCH_OPTIONS": {
+            "headless": True,
+        },
+        "DUPEFILTER_DEBUG": True,
     }
-    max_articles = 50
+    max_articles = 1000
     article_count = 0
 
     def start_requests(self):
@@ -31,50 +32,44 @@ class CnnSpider(scrapy.Spider):
 
     async def parse(self, response):
         page = response.meta["playwright_page"]
+        cards_locator = page.locator('div[data-editable="cards"]')
 
         while True:
-            await page.wait_for_selector('div[data-component-name="card"]')
+            await cards_locator.wait_for()
 
             article_links = [
                 await e.get_attribute("data-open-link")
-                for e in await page.locator('div[data-component-name="card"]').all()
+                for e in await cards_locator.locator(
+                    'div[data-component-name="card"]'
+                ).all()
             ]
 
             logging.info(f"Found {len(article_links)} article links")
 
             for link in article_links:
+                logging.info(f"Operating on article: {link}")
                 yield scrapy.Request(
                     response.urljoin(link),
                     self.parse_article,
-                    meta={
-                        "playwright": True,
-                        "playwright_page_methods": {
-                            "published_date": PageMethod(
-                                "evaluate", "window.CNN.contentModel.published_date"
-                            ),
-                            "last_updated_date": PageMethod(
-                                "evaluate", "window.CNN.contentModel.last_updated_date"
-                            ),
-                        },
-                    },
                 )
+
+                self.article_count += 1
 
                 if self.article_count >= self.max_articles:
                     logging.info(f"Reached max articles limit: {self.max_articles}")
                     await page.close()
                     return
 
-                self.article_count += 1
-
-            next_button = page.locator("div.pagination-arrow-right.text-active")
-            # FIXME: That's not how it works at all, next_button will never be None. Come up with a better way for checking this.
-            if next_button is None:
+            try:
+                await page.locator("div.pagination-arrow-right.text-active").click()
+            except playwright.async_api.TimeoutError:
                 logging.info("No more pages, closing")
                 await page.close()
                 return
 
             logging.info("Following next page")
-            await next_button.click()
+            # HACK: ensure that the next page has loaded in properly
+            await page.wait_for_timeout(5000)
 
     async def errback_close_page(self, failure):
         page = failure.request.meta["playwright_page"]
@@ -85,21 +80,23 @@ class CnnSpider(scrapy.Spider):
 
         title = response.css("h1#maincontent::text").get().strip()
         content = " ".join(
-            map(
-                str.strip,
-                response.css(
-                    'p.paragraph[data-component-name="paragraph"]::text'
-                ).getall(),
-            )
+            p.strip()
+            for p in response.css(
+                'p.paragraph[data-component-name="paragraph"]::text'
+            ).getall()
         )
-        page_methods = response.meta["playwright_page_methods"]
 
         logging.info(f"Found relevant article: {title}")
+
+        # Locate the script block that sets window.CNN metadata
+        script_block = response.xpath(
+            "/html/head/script[contains(.,'window.CNN = ')]"
+        ).get()
+        published_date = re.search("published_date: '(.*)',", script_block).group(1)
 
         yield {
             "title": title,
             "url": response.url,
             "content": content,
-            "published_date": page_methods["published_date"].result,
-            "last_updated_date": page_methods["last_updated_date"].result,
+            "published_date": published_date,
         }
